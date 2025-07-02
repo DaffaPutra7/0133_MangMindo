324eabc064ab5f73b714d85206e92fae9ab0d503
DaffaPutra7 <putradika3604@gmail.com>
2025-07-03 06:42:16 +0700

    membuat folder untuk constants yang berisikan file colors dan constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,13 @@
+// lib/core/constants/colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryRed = Color(0xffD92027);
+  static const Color primaryYellow = Color(0xffFFD700);
+  static const Color primaryGreen = Color(0xff28A745);
+
+  static const Color black = Color(0xff1D1D1D);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xff8D8D8D);
+  static const Color lightGrey = Color(0xffF4F4F4);
+}
