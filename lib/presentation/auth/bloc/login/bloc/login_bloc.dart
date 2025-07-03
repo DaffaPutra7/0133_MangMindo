8d6168a97b1e05f18f89e1dab70ebb15f601d2fd
DaffaPutra7 <putradika3604@gmail.com>
2025-07-03 22:14:48 +0700

    menghubungkan UI, event, state, dan repository di LoginBloc

---
+++ lib/presentation/auth/bloc/login/bloc/login_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projek_akhir/data/model/request/auth/login_request_model.dart';
+import 'package:projek_akhir/data/model/response/auth/auth_response_model.dart';
+import 'package:projek_akhir/data/repository/auth_repository.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository repository;
+
+  LoginBloc(this.repository) : super(LoginInitial()) {
+    on<LoginButtonPressed>((event, emit) async {
+      emit(LoginLoading());
+      // Memanggil method login yang general dari repository
+      final result = await repository.login(event.data);
+      result.fold(
+        (failure) => emit(LoginFailure(message: failure)),
+        (success) => emit(LoginSuccess(authResponse: success)),
+      );
+    });
+  }
+}
