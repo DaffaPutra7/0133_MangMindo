9f21a477d9ef186dc1e862ad9cc4f394d2040a95
DaffaPutra7 <putradika3604@gmail.com>
2025-07-11 16:04:43 +0700

    menambahkan method post

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
 class ServiceHttpClient {
   // baseUrl untuk koneksi dari emulator Android ke localhost
-  final String baseUrl = 'http://10.0.2.2:8000/api'; 
+  final String baseUrl = 'http://10.0.2.2:8000/api';
   final _secureStorage = const FlutterSecureStorage();
 
   // Method POST
@@ -36,5 +37,31 @@ class ServiceHttpClient {
         if (token != null) 'Authorization': 'Bearer $token',
       },
     );
+  }
+
+  // Method POST dengan file
+  Future<http.Response> postWithFile(
+    String endpoint,
+    Map<String, String> body,
+    XFile? file) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+    final token = await _secureStorage.read(key: 'auth_token');
+
+    var request = http.MultipartRequest('POST', url);
+
+    // Tambahkan header
+    request.headers['Authorization'] = 'Bearer $token';
+    request.headers['Accept'] = 'application/json';
+
+    // Tambahkan data teks
+    request.fields.addAll(body);
+
+    // Tambahkan file gambar jika ada
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath('image', file.path));
+    }
+
+    final streamedResponse = await request.send();
+    return await http.Response.fromStream(streamedResponse);
   }
 }
