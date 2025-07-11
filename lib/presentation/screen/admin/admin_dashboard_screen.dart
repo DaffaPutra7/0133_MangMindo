874972f192237f5361411824086e5a741ed96e80
DaffaPutra7 <putradika3604@gmail.com>
2025-07-11 17:46:28 +0700

    menambah navigasi untuk ke halaman AddMenuScreen

--- lib/presentation/screen/admin/admin_dashboard_screen.dart
+++ lib/presentation/screen/admin/admin_dashboard_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projek_akhir/core/core.dart';
 import 'package:projek_akhir/data/repository/admin_menu_repository.dart';
 import 'package:projek_akhir/presentation/admin/menu/bloc/menu_bloc.dart';
+import 'package:projek_akhir/presentation/screen/admin/menu/add_menu_screen.dart';
 import 'package:projek_akhir/service/service_http_client.dart';
 
 class AdminDashboardPage extends StatefulWidget {
@@ -16,9 +17,10 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => MenusBloc(
-        AdminMenuRepository(ServiceHttpClient()),
-      )..add(GetMenu()), // Langsung panggil event untuk mengambil data
+      create:
+          (context) =>
+              MenusBloc(AdminMenuRepository(ServiceHttpClient()))
+                ..add(GetMenu()), // Langsung panggil event untuk mengambil data
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Dashboard Admin - Menu'),
@@ -51,18 +53,19 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
                       leading: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         // Tampilkan gambar jika ada, jika tidak tampilkan ikon
-                        child: menu.image != null
-                            ? Image.network(
-                                // TODO: Ganti dengan URL gambar dari server Anda jika sudah production
-                                'http://10.0.2.2:8000/storage/menus/${menu.image!.split('/').last}',
-                                width: 60,
-                                height: 60,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(Icons.fastfood, size: 40);
-                                },
-                              )
-                            : const Icon(Icons.fastfood, size: 40),
+                        child:
+                            menu.image != null
+                                ? Image.network(
+                                  // TODO: Ganti dengan URL gambar dari server Anda jika sudah production
+                                  'http://10.0.2.2:8000/storage/menus/${menu.image!.split('/').last}',
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return const Icon(Icons.fastfood, size: 40);
+                                  },
+                                )
+                                : const Icon(Icons.fastfood, size: 40),
                       ),
                       title: Text(
                         menu.name,
@@ -70,7 +73,9 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
                       ),
                       subtitle: Text(
                         // Format harga ke Rupiah
-                        int.parse(menu.price.toString().split('.').first).currencyFormatRp,
+                        int.parse(
+                          menu.price.toString().split('.').first,
+                        ).currencyFormatRp,
                         style: const TextStyle(color: AppColors.primaryGreen),
                       ),
                       trailing: Row(
@@ -83,7 +88,10 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
                             },
                           ),
                           IconButton(
-                            icon: const Icon(Icons.delete, color: AppColors.primaryRed),
+                            icon: const Icon(
+                              Icons.delete,
+                              color: AppColors.primaryRed,
+                            ),
                             onPressed: () {
                               // TODO: Tampilkan dialog konfirmasi hapus
                             },
@@ -101,6 +109,10 @@ class _AdminDashboardPageState extends State<AdminDashboardPage> {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             // Navigasi ke halaman AddMenuPage saat tombol ditekan
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddMenuscreen()),
+            );
           },
           child: const Icon(Icons.add),
         ),
