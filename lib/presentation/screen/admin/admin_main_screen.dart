f14d16b1f93f1f30d29f92e85abf95cb7801e066
DaffaPutra7 <putradika3604@gmail.com>
2025-07-13 04:52:40 +0700

    menambah appbar dan fungsi logout untuk kembali ke halaman login

--- lib/presentation/screen/admin/admin_main_screen.dart
+++ lib/presentation/screen/admin/admin_main_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projek_akhir/core/constants/colors.dart';
 import 'package:projek_akhir/presentation/admin/menu/bloc/menu_bloc.dart';
+import 'package:projek_akhir/presentation/auth/login/bloc/login_bloc.dart';
 import 'package:projek_akhir/presentation/screen/admin/admin_dashboard_screen.dart';
 import 'package:projek_akhir/presentation/screen/admin/admin_order_screen.dart';
 import 'package:projek_akhir/presentation/screen/admin/menu/add_menu_screen.dart';
+import 'package:projek_akhir/presentation/screen/login_screen.dart';
 
 class AdminMainScreen extends StatefulWidget {
   const AdminMainScreen({super.key});
@@ -18,7 +21,7 @@ class _AdminMainScreenState extends State<AdminMainScreen> {
   // Daftar halaman yang akan dinavigasi
   final List<Widget> _pages = [
     const AdminDashboardPage(), // Halaman untuk kelola menu
-    const AdminOrderScreen(),     // Halaman untuk kelola pesanan
+    const AdminOrderScreen(), // Halaman untuk kelola pesanan
   ];
 
   void _onItemTapped(int index) {
@@ -29,42 +32,90 @@ class _AdminMainScreenState extends State<AdminMainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_selectedIndex == 0
-            ? 'Dashboard Admin - Menu'
-            : 'Dashboard Admin - Pesanan'),
-      ),
-      body: _pages[_selectedIndex], 
-      floatingActionButton: _selectedIndex == 0
-          ? FloatingActionButton(
-            onPressed: () async {
-              final result = await Navigator.push(
-                context, 
-                MaterialPageRoute(builder: (context) => const AddMenuscreen()),
-              );
-              if (result == true) {
-                context.read<MenusBloc>().add(GetMenu());
-              }
-            },
-            child: const Icon(Icons.add),
-          )
-        : null, // Sembunyikan tombol jika bukan di halaman menu
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            label: 'Menu',
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is LoginInitial) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            (route) => false,
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            _selectedIndex == 0
+                ? 'Dashboard Admin - Menu'
+                : 'Dashboard Admin - Pesanan',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long),
-            label: 'Pesanan',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+          backgroundColor: AppColors.primaryRed,
+          foregroundColor: AppColors.white,
+          actions: [
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder:
+                      (context) => AlertDialog(
+                        title: const Text('Logout'),
+                        content: const Text('Apakah Anda yakin ingin logout?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Batal'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              context.read<LoginBloc>().add(
+                                LogoutButtonPressed(),
+                              );
+                            },
+                            child: const Text(
+                              'Logout',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                );
+              },
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
+        body: _pages[_selectedIndex],
+        floatingActionButton:
+            _selectedIndex == 0
+                ? FloatingActionButton(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AddMenuscreen(),
+                      ),
+                    );
+                    if (result == true) {
+                      context.read<MenusBloc>().add(GetMenu());
+                    }
+                  },
+                  child: const Icon(Icons.add),
+                )
+                : null, // Sembunyikan tombol jika bukan di halaman menu
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.fastfood), label: 'Menu'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long),
+              label: 'Pesanan',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.grey,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
