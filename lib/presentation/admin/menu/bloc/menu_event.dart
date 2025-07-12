61da7c374f663c036e7aa1195c86e42e15aeccbf
DaffaPutra7 <putradika3604@gmail.com>
2025-07-13 00:58:48 +0700

    menambah event DeleteMenu

--- lib/presentation/admin/menu/bloc/menu_event.dart
+++ lib/presentation/admin/menu/bloc/menu_event.dart
@@ -3,3 +3,9 @@ part of 'menu_bloc.dart';
 abstract class MenuEvent {}
 
 class GetMenu extends MenuEvent {}
+
+// Event baru untuk memicu proses hapus
+class DeleteMenu extends MenuEvent {
+  final int menuId;
+  DeleteMenu({required this.menuId});
+}
