19db26288abc7e92d6a2570a19920a49fdfa7a09
DaffaPutra7 <putradika3604@gmail.com>
2025-07-14 00:43:38 +0700

    menambahkan method getOrderRiwayat untuk mengambil data riwayat pesanan

--- lib/data/repository/order_repository.dart
+++ lib/data/repository/order_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:dartz/dartz.dart';
 import 'package:projek_akhir/data/model/request/user/order_request_model.dart';
+import 'package:projek_akhir/data/model/response/user/order_riwayat_model.dart';
 import 'package:projek_akhir/service/service_http_client.dart';
 
 class OrderRepository {
@@ -14,7 +15,25 @@ class OrderRepository {
       if (response.statusCode == 201) {
         return const Right(true);
       } else {
-        final error = jsonDecode(response.body)['message'] ?? 'Gagal membuat pesanan';
+        final error =
+            jsonDecode(response.body)['message'] ?? 'Gagal membuat pesanan';
+        return Left(error);
+      }
+    } catch (e) {
+      return Left('Terjadi kesalahan: ${e.toString()}');
+    }
+  }
+
+  Future<Either<String, OrderRiwayatResponseModel>> getOrderRiwayat() async {
+    try {
+      // Panggil endpoint GET /orders
+      final response = await _httpClient.get('orders');
+
+      if (response.statusCode == 200) {
+        return Right(OrderRiwayatResponseModel.fromJson(response.body));
+      } else {
+        final error =
+            jsonDecode(response.body)['message'] ?? 'Gagal mengambil data';
         return Left(error);
       }
     } catch (e) {
